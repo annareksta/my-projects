4a7fe7fbd69839c76beb47a4ef8786e855fb4a13
annareksta <anna.reksta@gmail.com>
2025-03-18 17:13:52 +0200

    initial commit for all projects

---
+++ components/header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/github">GitHub Search</Link></li>
+        <li><Link href="/spotify">Spotify API</Link></li>
+        <li><Link href="/recipes">Recipes</Link></li>
+        <li><Link href="/weather">Weather</Link></li>
+      </ul>
+    </nav>
+  );
+}
